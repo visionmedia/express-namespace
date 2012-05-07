0e211c81b8b978b1bb5670c285bcf8a384e34c8f
Jean Ponchon <jean.ponchon@novadiscovery.fr>
2012-05-07 15:57:37 +0200

    New test suite using mocha and should

--- test/namespace.test.js
+++ test/namespace.test.js
@@ -2,21 +2,29 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , assert = require('assert')
-  , namespace = require('../');
+var express   = require('express')
+  , namespace = require('../')
+  , assert    = require('assert')
+  , should    = require('should')
+  , request   = require('./support/http');
 
-module.exports = {
-  'test app.namespace(str, fn)': function(){
-    var app = express()
-      , id;
 
-    app.get('/one', function(req, res){
-      res.send('GET one');
-    });
 
-    assert.equal(app.namespace('/user', function(){}), app);
+describe('app.namespace(str, fn)', function(){
 
+  var app = express()
+    , id;
+
+  app.get('/one', function(req, res){
+    res.send('GET one');
+  });
+
+  it('app should be equal after namespace call', function() {
+    app.namespace('/user', function(){}).should.equal(app);
+  });
+
+
+  it('should add namespace', function() {
     app.namespace('/user', function(){
       app.all('/:id', function(req, res, next){
         id = req.params.id;
@@ -35,165 +43,252 @@ module.exports = {
     app.get('/two', function(req, res){
       res.send('GET two');
     });
+  });
 
-    assert.response(app,
-      { url: '/user/12' },
-      { body: 'GET user 12' });
-  
-    assert.response(app,
-      { url: '/user/12', method: 'DELETE' },
-      { body: 'DELETE user 12' });
-    
-    assert.response(app,
-      { url: '/one' },
-      { body: 'GET one' });
-    
-    assert.response(app,
-      { url: '/two' },
-      { body: 'GET two' });
-  },
-  
-  'test app.namespace(str, fn) nesting': function(done){
-    var pending = 6
-      , calls = 0
-      , app = express();
-    
-    function finished() {
-      --pending || function(){
-        assert.equal(2, calls);
+
+  it('should get() namespaced route', function(done) {
+    request(app)
+      .get('/user/12')
+      .end(function(res) {
+        res.body.should.equal('GET user 12');
         done();
-      }();
-    }
-
-    function middleware(req, res, next) {
-      ++calls;
-      next();
-    }
-
-    app.get('/one', function(req, res){
-      res.send('GET one');
-    });
-
-    app.namespace('/forum/:id', function(){
-      app.get('/', function(req, res){
-        res.send('GET forum ' + req.params.id);
       });
-      
-      app.get('/edit', function(req, res){
-        res.send('GET forum ' + req.params.id + ' edit page');
-      });
+  });
 
-      app.namespace('/thread', function(){
-        app.get('/:tid', middleware, middleware, function(req, res){
-          res.send('GET forum ' + req.params.id + ' thread ' + req.params.tid);
-        });
-      });
-
-      app.del('/', function(req, res){
-        res.send('DELETE forum ' + req.params.id);
-      });
-    });
-    
-    app.get('/two', function(req, res){
-      res.send('GET two');
-    });
-
-    assert.response(app,
-      { url: '/forum/1' },
-      { body: 'GET forum 1' }
-      , finished);
-    
-    assert.response(app,
-      { url: '/forum/1/edit' },
-      { body: 'GET forum 1 edit page' }
-      , finished);
-    
-    assert.response(app,
-      { url: '/forum/1/thread/50' },
-      { body: 'GET forum 1 thread 50' }
-      , finished);
-  
-    assert.response(app,
-      { url: '/forum/2', method: 'DELETE' },
-      { body: 'DELETE forum 2' }
-      , finished);
-    
-    assert.response(app,
-      { url: '/one' },
-      { body: 'GET one' }
-      , finished);
-    
-    assert.response(app,
-      { url: '/two' },
-      { body: 'GET two' }
-      , finished);
-  },
-  
-  'test fn.route': function(){
-    var app = express();
-
-    app.namespace('/user/:id', function(){
-      app.get('/', function handler(req, res){
-        assert.equal('/user/:id', handler.namespace);
-        res.send(200);
-      });
-    });
-
-    assert.response(app,
-      { url: '/user/12' },
-      { body: 'OK' });
-  },
-  'test app.namespace(str, middleware, fn)': function(done){
-    var app = express(),
-        calledA = 0,
-        calledB = 0;
-    
-    function middlewareA(req,res,next){
-      calledA++;
-      next();
-    }
-
-    function middlewareB(req,res,next){
-      calledB++;
-      next();
-    }
-
-    app.namespace('/user/:id', middlewareA, function(){
-      app.get('/', function(req,res){
-        res.send('got Home');
-      });
-
-      app.get('/other', function(req,res){
-        res.send('got Other');
-      });
-
-      app.namespace('/nest', middlewareB, function(req,res){
-        app.get('/', function(req,res){
-          res.send('got Nest');
-        });
-      });
-    });
-
-    var pending = 3;
-    function finished() {
-      --pending || function(){
-        assert.equal(3, calledA);
-        assert.equal(1, calledB);
+  it('should delete() namespaced route', function(done) {
+    request(app)
+      .delete('/user/12')
+      .end(function(res) {
+        res.body.should.equal('DELETE user 12');
         done();
-      }();
-    }
+      });
+  });
 
-    assert.response(app,
-      { url: '/user/12' },
-      { body: 'got Home' }, 
-      finished);
-    assert.response(app,
-      { url: '/user/12/other' },
-      { body: 'got Other' },
-      finished);
-    assert.response(app,
-      { url: '/user/12/nest' },
-      { body: 'got Nest' },
-      finished);
+
+  it('should get() a route defined before namespaced routes', function(done) {
+    request(app)
+      .get('/one')
+      .end(function(res) {
+        res.body.should.equal('GET one');
+        done();
+      });
+  });
+
+  it('should get() a route defined after namespaced routes', function(done) {
+    request(app)
+      .get('/two')
+      .end(function(res) {
+        res.body.should.equal('GET two');
+        done();
+      });
+  });
+
+});
+
+
+
+
+describe('app.namespace(str, fn) nesting', function(){
+  var pending = 6
+    , calls = 0
+    , app = express();
+  
+  function finished() {
+    --pending || function(){
+      assert.equal(2, calls);
+    }();
   }
-};
+
+  function middleware(req, res, next) {
+    ++calls;
+    next();
+  }
+
+  app.get('/one', function(req, res){
+    res.send('GET one');
+  });
+
+  app.namespace('/forum/:id', function(){
+    app.get('/', function(req, res){
+      res.send('GET forum ' + req.params.id);
+    });
+    
+    app.get('/edit', function(req, res){
+      res.send('GET forum ' + req.params.id + ' edit page');
+    });
+
+    app.namespace('/thread', function(){
+      app.get('/:tid', middleware, middleware, function(req, res){
+        res.send('GET forum ' + req.params.id + ' thread ' + req.params.tid);
+      });
+    });
+
+    app.del('/', function(req, res){
+      res.send('DELETE forum ' + req.params.id);
+    });
+  });
+  
+  app.get('/two', function(req, res){
+    res.send('GET two');
+  });
+
+
+  it('should get() /forum/1', function(done) {
+    request(app)
+      .get('/forum/1')
+      .end(function(res) {
+        res.body.should.equal('GET forum 1');
+        finished();
+        done();
+      });
+  });
+
+  it('should get() /forum/1/edit', function(done) {
+    request(app)
+      .get('/forum/1/edit')
+      .end(function(res) {
+        res.body.should.equal('GET forum 1 edit page');
+        finished();
+        done();
+      });
+  });
+
+  it('should get() /forum/1/thread/50', function(done) {
+    request(app)
+      .get('/forum/1/thread/50')
+      .end(function(res) {
+        res.body.should.equal('GET forum 1 thread 50');
+        finished();
+        done();
+      });
+  });
+
+  it('should delete() /forum/2', function(done) {
+    request(app)
+      .delete('/forum/2')
+      .end(function(res) {
+        res.body.should.equal('DELETE forum 2');
+        finished();
+        done();
+      });
+  });
+
+
+  it('should get() a route defined before namespaced routes', function(done) {
+    request(app)
+      .get('/one')
+      .end(function(res) {
+        res.body.should.equal('GET one');
+        finished();
+        done();
+      });
+  });
+
+  it('should get() a route defined after namespaced routes', function(done) {
+    request(app)
+      .get('/two')
+      .end(function(res) {
+        res.body.should.equal('GET two');
+        finished();
+        done();
+      });
+  });
+
+});
+
+
+
+
+describe('fn.route', function(){
+  var app = express();
+
+  app.namespace('/user/:id', function(){
+    app.get('/', function handler(req, res){
+      assert.equal('/user/:id', handler.namespace);
+      res.send(200);
+    });
+  });
+
+  it('should get() /user/12', function(done) {
+    request(app)
+      .get('/user/12')
+      .end(function(res) {
+        res.body.should.equal('OK');
+        done();
+      });
+  });
+
+});
+
+describe('app.namespace(str, middleware, fn)', function(){
+  var app = express(),
+      calledA = 0,
+      calledB = 0;
+  
+  function middlewareA(req,res,next){
+    calledA++;
+    next();
+  }
+
+  function middlewareB(req,res,next){
+    calledB++;
+    next();
+  }
+
+  app.namespace('/user/:id', middlewareA, function(){
+    app.get('/', function(req,res){
+      res.send('got Home');
+    });
+
+    app.get('/other', function(req,res){
+      res.send('got Other');
+    });
+
+    app.namespace('/nest', middlewareB, function(req,res){
+      app.get('/', function(req,res){
+        res.send('got Nest');
+      });
+    });
+  });
+
+  var pending = 3;
+  function finished() {
+    --pending || function(){
+      assert.equal(3, calledA);
+      assert.equal(1, calledB);
+    }();
+  }
+
+
+  it('should get() /user/12', function(done) {
+    request(app)
+      .get('/user/12')
+      .end(function(res) {
+        res.body.should.equal('got Home');
+        finished();
+        done();
+      });
+  });
+
+  it('should get() /user/12/other', function(done) {
+    request(app)
+      .get('/user/12/other')
+      .end(function(res) {
+        res.body.should.equal('got Other');
+        finished();
+        done();
+      });
+  });
+
+  it('should get() /user/12/nest', function(done) {
+    request(app)
+      .get('/user/12/nest')
+      .end(function(res) {
+        res.body.should.equal('got Nest');
+        finished();
+        done();
+      });
+  });
+
+});
+
